9c1b93987047341465d433c16733210349fb20a0
AkmalbekDEV <akmalyusufov32@gmail.com>
2024-08-27 18:01:24 +0500

    some changes

--- src/pages/Even09.jsx
+++ src/pages/Even09.jsx
@@ -19,7 +19,7 @@ const Even09 = () => {
 
     return (
         <div className='max-w-[1250px] mx-auto mt-36 max-sm:px-5'>
-            <h1 className='text-2xl font-medium max-sm:text-center text-blue-700 shadow-xl rounded-xl p-5 max-sm:text-lg'><span className='text-red-500 text-3xl max-sm:text-xl'>Eslatma:</span> Agar Uyga vazifani siz 3 marotabadan ko'p bajarmasangiz yoki 1 oy davomida Vocabularyda 70 dan ko'p xato qilsangiz, qizil ro'yxatga tushasiz va imtihondan chetlashtirilasiz. Qizil ro'yxatga tushgan o'quvchi o'z ismini rangidan bilib olishi mumkin.</h1>
+            <h1 className='text-2xl font-medium max-sm:text-center text-blue-700 shadow-xl rounded-xl p-5 max-sm:text-lg'><span className='text-red-500 text-3xl max-sm:text-xl'>Eslatma:</span> Agarda siz uyga vazifani 3 marotabadan ko’p bajarmagan bo’lsangiz yoki Vocabulary dan xatolaringiz 70 tadan oshsa siz imtihonga qo’yilmaysiz. Iltimos homework larni vaqtida bajarib Vocabulary ni yodlab yuring!!!</h1>
             <table className="min-w-full text-center text-xl max-sm:text-lg font-medium mt-10 border-blue-600 border-b-2">
                 <thead
                     className="border-b bg-blue-700 font-medium text-white text-xl max-sm:text-lg">
@@ -43,8 +43,7 @@ const Even09 = () => {
                                     <td className="whitespace-nowrap  px-6 max-sm:px-3 py-4 text-red-500">{data.vocabulary} tasi yozilmagan (Jami)</td>
                                 </tr>
                             )
-                        }
-                        else {
+                        } else {
                             return (
                                 <tr key={data.id} className="border-b-2 border-blue-600">
                                     <td className="whitespace-nowrap  px-6 max-sm:px-3 py-4 font-medium">{data.id}</td>
